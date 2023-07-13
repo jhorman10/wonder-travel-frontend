dea7878111ea7c9203a6dc269fd8b85c58952d8f
Jhorman Orozco <jsojhor@gmail.com>
2023-07-13 12:42:31 -0500

    add PWA configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react-swc';
+
+const manifestForPlugin = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  manifest: {
+    name: 'Wonder Travel',
+    short_name: 'Wonder',
+    description: 'An app that can you schedule a lot of travels.',
+    icons: [
+      {
+        src: '/src/assets/images/Group 1098.svg',
+        sizes: '192x192',
+        type: 'image/svg',
+      },
+    ],
+    theme_color: '#171717',
+    background_color: '#e8ebf2',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
