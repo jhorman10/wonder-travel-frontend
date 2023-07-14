0e70fb2aa103880b813ee643a7127d1410345543
Jhorman Orozco <jsojhor@gmail.com>
2023-07-14 11:23:49 -0500

    PWA config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,27 @@
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
-import react from '@vitejs/plugin-react-swc';
-
-const manifestForPlugin = {
-  registerType: 'prompt',
-  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-  manifest: {
-    name: 'Wonder Travel',
-    short_name: 'Wonder',
-    description: 'An app that can you schedule a lot of travels.',
-    icons: [
-      {
-        src: '/src/assets/images/Group 1098.svg',
-        sizes: '192x192',
-        type: 'image/svg',
-      },
-    ],
-    theme_color: '#171717',
-    background_color: '#e8ebf2',
-    display: 'standalone',
-    scope: '/',
-    start_url: '/',
-    orientation: 'portrait',
-  },
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), VitePWA(manifestForPlugin)],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Wonder Travel',
+        short_name: 'Wonder',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/src/assets/images/Group 1098.svg',
+            sizes: '192x192',
+            type: 'image/svg',
+          },
+        ],
+      },
+    }),
+  ],
 });
